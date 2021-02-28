fefb8bebc6d98ec5356b4943f47d75aa9b4f9555
Robert G <robertrover811@outlook.com>
2021-02-28 05:41:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function randomAdvice(){
+  advice = ['Ride a bike to school/work', 'turn off the faucet when not using it', 'Recycle recycleables'];
+
+  randomValue = advice[Math.floor(Math.random() * advice.length)];
+  
+  console.log(randomValue);
+  alert(randomValue);
+}
+
+
+
+
+
+
