787947a6475a65331e208f3f03b9d59e4a8884c2
Robert G <robertrover811@outlook.com>
2021-02-28 20:11:28 +0000

    mainly css changes. implemented flexbox and got started on firebase

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+//$(document).ready(function() {
+   // This is where our code will go
+//});
+
 function randomAdvice(){
-  advice = ['Ride a bike to school/work', 'turn off the faucet when not using it', 'Recycle recycleables'];
+  advice = ['Dont leave your water running', 'Use recycled bags while shopping', 'Ride your bike, walk, or use public transportation instead of driving a car', 'Recycle and compost', 'Use natural cleaning products', 'Switch off appliances when not in use', 'Unplug your charger when your device is fully charged', 'Wash your clothes at low temperature', 'Use short cycles for washing clothes', 'Fix leaks', 'Take short showers', 'Turn off the tap while brushing your teeth', 'Choose local seasonal produce', 'Reduce the amount of meals you eat containing meat', 'Avoid bottled water', 'Buy recycled paper', 'Grow food', 'Plant a tree', 'Dont litter'];
 
   randomValue = advice[Math.floor(Math.random() * advice.length)];
   
@@ -7,8 +11,18 @@ function randomAdvice(){
   alert(randomValue);
 }
 
+dictionary = {
+  'first':["Robert Goll", 4],
+  'second':["Louis Sansevero", 3],
+  'third':["Simone Zabid", 2],
+  'fourth':["Cyrus Kottmeier", 1]
+};
 
-
-
-
-
+var Robert_Goll = 4;
+console.log(Robert_Goll);
+console.log(dictionary);
+console.log(dictionary.first[0])
+//var x = document.createElement("P");
+//var t = document.createTextNode(dictionary.first[0]);
+//x.appendChild(t);
+//document.body.appendChild(x);
